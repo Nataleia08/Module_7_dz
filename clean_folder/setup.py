a9895a179536e73cd6e99f60e20ac21f912ab56a
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2022-11-27 19:19:39 +0200

    Commit 27_11_1
    Draft

---
+++ clean_folder/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='clean_folder',
+    version='1',
+    description='Very useful code',
+    url='https://github.com/Nataleia08/Module_7_dz',
+    author='Orlovska Nataliia',
+    author_email='nataleia.orlovska@gmail.com',
+    license='MIT',
+    packages=find_namespace_packages(),
+    # install_requires=['markdown'],
+    entry_points={'console_scripts': [
+        'clean-folder = clean_folder.clean']}
+)
